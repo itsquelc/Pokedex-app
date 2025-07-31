bb135420c47cf2bf23ee26d045a5d314b014d6b0
itsquelc <raquelnunescirino@gmail.com>
2025-07-31 11:48:28 -0300

    52

---
+++ pokedex/components/Card/index.jsx
@@ -0,0 +1,27 @@
+import { Image, View, Text, ScrollView, TouchableOpacity } from "react-native";
+import { POKEMON_TYPE_COLORS } from "../../utils/colors";
+import { styles } from "./styles";
+import { Button } from "../Button";
+
+export default function Card({ pokemon }) {
+  return (
+    <TouchableOpacity
+      style={[styles.container, 
+        { backgroundColor: `${POKEMON_TYPE_COLORS[pokemon.Tipo[0].Nome]}` },
+      ]}>
+      <View style={styles.info}>
+        <Text style={styles.numero}>#{pokemon.Numero}</Text>
+        <Text style={styles.nome}>{pokemon.Nome}</Text>
+        <ScrollView horizontal>
+          {pokemon.Tipo.map((tipo) => (
+            <Button tipo={tipo} key={tipo.Nome} />
+          ))}
+        </ScrollView>
+      </View>
+
+      <View style={styles.imageContainer}>
+        <Image source={{ uri: pokemon.Imagem }} style={styles.imagem} />
+      </View>
+    </TouchableOpacity>
+  );
+}
